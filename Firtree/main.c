5cfb6793ef199603ca52157cfc8f1dee4a3c4ed1
Rémy Noulin <72010287+Waz0x@users.noreply.github.com>
2021-08-18 18:50:50 +0200

    MOD | rename variable

--- Firtree/main.c
+++ Firtree/main.c
@@ -17,7 +17,7 @@ void _t(int s)
     if (s == 0)return;
     l=_(s);
     for(int i=0,j=4,k=1;i<s;i++,j++){c+=(2*(j-k));(i%2)?0:(k+=1);}c++;
-    for(int i=0, b=1,bl=_(b),r=0;i<l;i++){if(i==bl){b+=1;bl=_(b);r+=(b%2?0:1);id-=(r+1);}for(int sp=0;sp<c/2-id;sp++)P(" ");for(int st=0;st<1+(id*2);star++)P("*");P("\n",id+=1);}
+    for(int i=0, b=1,bl=_(b),r=0;i<l;i++){if(i==bl){b+=1;bl=_(b);r+=(b%2?0:1);id-=(r+1);}for(int sp=0;sp<c/2-id;sp++)P(" ");for(int st=0;st<1+(id*2);st++)P("*");P("\n",id+=1);}
     for(int i=0,ss=s,e=(s%2)?s:s+1;i<s;i++){for(int sp=0;sp<(c/2-e/2);sp++){P(" ");}for(int p=0;p!=e;p++){P("|");}P("\n");}
 }
 
